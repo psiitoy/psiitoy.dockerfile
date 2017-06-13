0e216425a9b8be9e3002a128b6b3c9b9e3297694
psiitoy <psiitoy@126.com>
2017-06-13 17:54:05 +0800

    first commit

---
+++ dockerfile-alpine-es/docker-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 脚本文件开头加上set -e,这句语句告诉bash如果任何语句的执行结果不是true则应该退出
+set -e
+
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+# allow the container to be started with `--user`
+if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
+	# Change the ownership of user-mutable directories to elasticsearch
+	for path in \
+		/usr/share/elasticsearch/data \
+		/usr/share/elasticsearch/logs \
+	; do
+		chown -R elasticsearch:elasticsearch "$path"
+	done
+	
+	set -- su-exec elasticsearch "$@"
+	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
+fi
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
